15348dd0251409bf7a2503f0ef3df757db648454
d3vn0n <sochi98.sn@gmail.com>
2023-09-24 21:38:55 +0100

    Load env vars from file

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
 	fmt.Println("Hello World")
+
+	godotenv.Load()
+
+	portNum := os.Getenv("PORT")
+	if portNum == "" {
+		log.Fatal("PORT not found in your enironment")
+	}
+
+	fmt.Println("Port:", portNum)
 }
